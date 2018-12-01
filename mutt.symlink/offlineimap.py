d61f62dd30d19fc7001315035d076bd8bb14f681
Simon Bihel <simon.bihel@ens-rennes.fr>
2018-12-01 19:04:58 +0000

    Add protonmail

--- mutt.symlink/offlineimap.py
+++ mutt.symlink/offlineimap.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python3
+
+from socket import gethostname
 from subprocess import check_output
 
 
-def get_pass(account):
-    return check_output("pass Mail/" + account, shell=True).splitlines()[0]
+def get_pass(account, add_hostname=False):
+    if add_hostname:
+        account += "_" + gethostname().strip()
+    pass_output = check_output("pass Mail/" + account, shell=True) \
+        .splitlines()[0]
+    return pass_output.decode("utf-8").strip()
 
 
 if __name__ == "__main__":
     import sys
-    print(get_pass(sys.argv[1]))
+    if len(sys.argv) == 3:
+        print(get_pass(sys.argv[1], sys.argv[2]))
+    else:
+        print(get_pass(sys.argv[1]))
